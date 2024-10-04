3c760fac917a6c4de0a537e6acb4873d4522d663
Jack LoCasto <jacklocasto@gmail.com>
2024-10-03 22:40:23 -0400

    Able to get email messages and the content of the messages by id.

--- unsubscriber.py
+++ unsubscriber.py
@@ -9,9 +9,12 @@ from googleapiclient.errors import HttpError
 
 # readonly scope only necessary for reading message bodies
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+API_NAME = "gmail"
+API_VERSION = "v1"
+USER_EMAIL_ENDPOINT = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 
 
-def gmail_authenticate():
+def gmail_authenticate() -> Credentials:
     if os.path.exists("credentials.json") is False:
         print("File Not Found Error: \"credentials.json\" is missing. Please be sure to pull your credentials.json file from your Google Cloud account. See details here on how to generate this: https://developers.google.com/gmail/api/quickstart/python#authorize_credentials_for_a_desktop_application")
         exit()
@@ -35,6 +38,35 @@ def gmail_authenticate():
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
+    return creds
+
+
+def get_emails(creds: Credentials, filter: str = None) -> list:
+    # Call the Gmail API
+    service = build("gmail", "v1", credentials=creds)
+    results = ""
+    if filter is not None:
+        results = service.users().messages().list(userId="me", q=filter).execute()
+    else:
+        results = service.users().messages().list(userId="me").execute()
+
+    return results["messages"]
+
+
+def get_email_contents(creds: Credentials, email_id: str):
+    service = build("gmail", "v1", credentials=creds)
+    results = service.users().messages().get(userId="me", id=email_id).execute()
+    print(results["payload"]["parts"])
+
+
+def unsubscribe_scrape(creds: Credentials, email_ids: list):
+    # For each file in the inbox, click unsubscribe.
+    get_email_contents(creds=creds, email_id=email_ids[0]["id"])
+    # for email in email_ids:
+    #     get_email_contents(creds=creds, email_id=email["id"])
+    # No need to fill out a form - clicking unsubscribe works.
+    # Form needs to be filled out on unsubscribing site - these will likely be unique.
+
 
 def main(args: dict):
     # Make sure the file is being run from its current working directory
@@ -42,14 +74,17 @@ def main(args: dict):
     os.chdir(curr_file_path)
 
     # (1) Authorize and Authenticate into Gmail account
-    gmail_authenticate()
+    creds = gmail_authenticate()
+    if creds is None:
+        print("Credentials are not valid. Adjust your authentication code and rerun.")
+        exit()
     # (2) Filter email messages based on the user query provided
+    email_ids = get_emails(creds=creds, filter=args["filter"])
+    # (3) Scrape the email contents and unsubscribe to the marketing email
+    unsubscribe_scrape(creds=creds, email_ids=email_ids)
 
-    # (3) For each file in the inbox, click unsubscribe.
-    #   (3.1) No need to fill out a form - clicking unsubscribe works.
-    #   (3.2) Form needs to be filled out on unsubscribing site - these will likely be unique.
 
-
+# my filter:  "unsubscribe -{usps, linkedin, amazon web services, geico}"
 if __name__ == "__main__":
     # parse args
     parser = argparse.ArgumentParser(
